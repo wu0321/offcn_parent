6fd5d2acf46d917b61c239813180ebfb8df4cdd9
wu0321 <779987971@qq.com>
2021-04-20 20:01:16 +0800

    hhhahh

---
+++ scw-parent/scw-order/src/main/java/com/offcn/order/config/Tsfd.java
@@ -0,0 +1,14 @@
+package com.offcn.order.config;
+
+/**
+ * @ClassName Tsfd
+ * @Author Y
+ * @Date 2021/4/2020:00
+ * @Version 1.0
+ * @Description
+ **/
+public class Tsfd {
+    public static void main(String[] args) {
+        System.out.println(1111);
+    }
+}
